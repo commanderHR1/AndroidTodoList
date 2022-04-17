95170f66b306c54b842af48ef6ea2994d6506315
Harris Ransom <hransom528@gmail.com>
2022-04-16 21:34:19 -0400

    Added app layout

--- app/app/src/main/java/com/commanderhr1/todolist/MainActivity.kt
+++ app/app/src/main/java/com/commanderhr1/todolist/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.commanderhr1.todolist
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    
+    private var itemList = arrayListOf<String>()
+    var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, itemList)
+
+    // onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
